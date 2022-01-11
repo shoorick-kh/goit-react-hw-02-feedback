2b48c880fc21e88b78a0cc754587225b4e032a8f
Oleksandr Khrapak <al.khrapak@gmail.com>
2022-01-11 13:27:23 +0200

    add components & styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import FeedbackOptions from './components/feedbackOptions/FeedbackOptions';
+import Notification from './components/notification/Notification';
+import Section from './components/section/Section';
+import Statistics from './components/statistics/Statistics';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = { good: 0, neutral: 0, bad: 0 };
+
+  addFeedback = params => {
+    this.setState(prevState => {
+      return {
+        [params]: (prevState[params] += 1),
+      };
+    });
+  };
+
+  countTotalFeedback() {
+    return Object.values(this.state).reduce((acc, value) => acc + value, 0);
+  }
+
+  countPositiveFeedbackPercentage() {
+    return Math.round((this.state.good / this.countTotalFeedback()) * 100);
+  }
+
+  render() {
+    return (
+      <>
+        <Section title="Please leave feedback">
+          <FeedbackOptions params={this.state} addFeedback={this.addFeedback} />
+        </Section>
+
+        <Section title="Statistics">
+          {this.countTotalFeedback() > 0 ? (
+            <Statistics
+              params={this.state}
+              totalFeedback={this.countTotalFeedback()}
+              positiveFeedbackPercentage={this.countPositiveFeedbackPercentage()}
+            />
+          ) : (
+            <Notification message="There is no feedback" />
+          )}
+        </Section>
+      </>
+    );
+  }
 }
 
 export default App;
